5ce928166afe258d54f9ee9f2951f8f2890eec1e
Ansahmbom <ansahmbomyong@gmail.com>
2023-02-16 11:59:40 -0800

    a C programme that that prints the size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main: displays the size of specified variables
+ *
+ * Returns: 0
+ *
+ */
+int main(void)
+{
+	/* declaring variables */
+	char charType;
+	double doubleType;
+	int intType;
+	float floatType;
+
+	/* using sizeof to determine the size of variables */
+	printf("Size of char: %zu bytes\n", sizeof(charType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	reurn (0);
+}
