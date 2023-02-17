2239524716d62864a6a7c904e170138d23452953
Ansahmbom <ansahmbomyong@gmail.com>
2023-02-17 07:39:58 -0800

    add ding a script that assigns a random number to a variable and then prints whether the number is positive, negative or zero.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ * main - assigns a random number to variable n
+ * 	and then decides whether it is positive,
+ * 	negative, or zero.
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d\n is positive\n", n)
+	}
+	else
+	{
+		if (n < 0)
+		{
+			printf("%d\n is negative\n", n)
+		}
+		else
+		{
+			printf("%d\n is zero\n", n)
+		}
+	}
+	return (0);
+}
