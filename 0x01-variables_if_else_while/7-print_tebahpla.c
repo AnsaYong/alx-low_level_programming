9c295549511e6b881e7d8790e5211cb838f2d3da
Ansahmbom <ansahmbomyong@gmail.com>
2023-02-17 11:43:39 -0800

    a prog that prints the reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the reverse alphabet
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int al = 'z';
+
+	while (al >= 'a')
+	{
+		putchar(al);
+		al--;
+	}
+	putchar('\n');
+}
