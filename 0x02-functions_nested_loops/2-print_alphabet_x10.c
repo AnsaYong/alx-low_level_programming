7799bfee0198b2878f33d77303b1710bd6685295
Ansahmbom <ansahmbomyong@gmail.com>
2023-02-21 09:31:45 -0800

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (lett = 'a'; lett <= 'z'; lett++)
+		{
+			_putchar(lett);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
