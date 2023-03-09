27c2acdfac9bb84289eafc0959525cb67dfa9153
Ansahmbom <ansahmbomyong@gmail.com>
2023-03-09 15:04:01 -0800

    removing loop

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,15 +16,17 @@
 
 int sqrt_helper(int x, int left, int right)
 {
+	int mid, square, result;
+
+	mid = left + (right - left) / 2;
+	square = mid * mid;
+	result = sqrt_helper(x, mid + 1, right);
+	
 	if (left > right)
 	{
 		return (-1);  /* Return -1 if no integer square root exists */
 	}
 
-	int mid = left + (right - left) / 2;
-	int square = mid * mid;
-	int result = sqrt_helper(x, mid + 1, right);
-	
 	if (square == x)
 	{
 		return (mid);
