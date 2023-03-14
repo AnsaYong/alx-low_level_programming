f40e1bacfa1ee76bf2b313941f8f8d3e818ad175
Ansahmbom <ansahmbomyong@gmail.com>
2023-03-14 07:40:45 -0700

    functions for memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,10 +17,10 @@ char *str_concat(char *s1, char *s2)
 
 	l = 0;
 
-	if (*s1 == NULL)
-		*s1 = " ";
-	if (*s2 == NULL)
-		*s2 = " ";
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 
 	while (s1[i] != '\0')
 		i++;
