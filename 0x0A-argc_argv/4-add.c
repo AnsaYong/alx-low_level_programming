53db33557ea2f952668c88bf306640a23c42b1cd
AnsaYong <ansahmbomyong@gmail.com>
2023-06-09 16:07:37 +0200

    added a check to the add funtion to only add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	while (i < argc)
 	{
-		if (*argv[i] >= 48 && *argv[i] <= 57 && argv[i][0] != '-')
+		if (*argv[i] >= 48 && *argv[i] <= 57 && argv[i][0] != '-' && atoi(argv[i]) > 0)
 			sum += atoi(argv[i]);
 		else
 		{
