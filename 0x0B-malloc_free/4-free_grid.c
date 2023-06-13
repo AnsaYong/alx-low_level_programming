4d3b67672ae465f7d539af4db54d94dbdce23cc4
AnsaYong <ansahmbomyong@gmail.com>
2023-06-13 15:29:36 +0200

    adding a function that frees dynamiclly allocated memory for a 2D grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,7 +7,8 @@
  * @grid: dynamically allocated 2D grid
  * @height: number of rows in the grid
  */
-void free_grid(int **grid, int height){
+void free_grid(int **grid, int height)
+{
 	int i;
 
 	for (i = 0; i < height; i++)
