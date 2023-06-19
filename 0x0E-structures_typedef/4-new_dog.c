bef3d7cdf7117ada4e01819049ace91e607a3604
AnsaYong <ansahmbomyong@gmail.com>
2023-06-19 17:45:00 +0200

    updating 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog using dog_t
@@ -11,16 +12,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i, name_len, owner_len;
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-
-	new_dog->name = name;
+	/* copy name to newly created memory*/
+	name_len = strlen(name) + 1;
+	new_dog->name = malloc(name_len);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i < name_len; i++)
+		((*new_dog).name)[i] = name[i];
+	((*new_dog).name)[i] = '\0';
+	/* copy owner to newly allocated memory */
+	owner_len = strlen(owner) + 1;
+	new_dog->owner = malloc(owner_len);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_len; i++)
+		((*new_dog).owner)[i] = owner[i];
+	((*new_dog).owner)[i] = '\0';
 	new_dog->age = age;
-	new_dog->owner = owner;
 
 	return (new_dog);
 }
