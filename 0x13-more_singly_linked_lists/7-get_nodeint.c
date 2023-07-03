70846639dff8acbd8740a2661dfd51f8353e4dbe
AnsaYong <ansahmbomyong@gmail.com>
2023-07-03 23:27:10 +0200

    updating 7-get_nodeint.c to handle the case where index is greater than the number of elements in the provided linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
 	unsigned int n = 0;
+	size_t elts = listint_len(head);
 
 	if (head == NULL)
+		return (NULL);
+
+	if (elts >= index)
 		return (NULL);
 
 	temp = head;
