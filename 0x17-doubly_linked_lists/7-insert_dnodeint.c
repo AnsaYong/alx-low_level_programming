a2cec080badd9ff0ccbaaa0ab5e4d6df94279bc1
AnsaYong <ansahmbomyong@gmail.com>
2023-09-15 02:35:08 +0200

    updating 7-insert_dnodeint.c to handle the edge case where index is at start of list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,42 +11,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *prev_node;
-	dlistint_t *next_node;
-	dlistint_t *new_node;
+	dlistint_t *prev_node = NULL;
+	dlistint_t *new_node = NULL;
 	unsigned int i = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)	/* special case for inserting at the begining */
+		new_node = add_dnodeint(h, n);
+
 	/* create new node and add its data */
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	/* remember you don't have to mess with the head */
+
+	/* Traverse the list to find the given index */
 	prev_node = *h;
-	next_node = prev_node->next;
-	/* find the given index */
-	while (i < idx)
+	while (prev_node != NULL && i < idx - 1)
 	{
-		prev_node = next_node;
-		/* check if end of list and idx is not next i.e out of bounds */
-		if (next_node == NULL && i < (idx - 1))
-			return (NULL);
-		next_node = prev_node->next;
+		prev_node = prev_node->next;
 		i++;
 	}
-	/* check if end of list */
-	if (next_node == NULL)
+
+	if (prev_node == NULL)	/* check if index is out of bounds */
 	{
-		new_node->next = NULL;
-		new_node->prev = prev_node;
-		prev_node->next = new_node;
+		free(new_node);
+		return (NULL);
 	}
-	else
-	{
-		new_node->next = next_node;
-		next_node = new_node;
-		new_node->prev = prev_node;
-		if (next_node->next != NULL)
-			next_node->prev = new_node;
-	}
+
+	/* Update pointers to insert the new node */
+	new_node->next = prev_node->next;
+	new_node->prev = prev_node;
+	if (prev_node->next != NULL)
+		prev_node->next->prev = new_node;
+	prev_node->next = new_node;
+
 	return (new_node);
 }
